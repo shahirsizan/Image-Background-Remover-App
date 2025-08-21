0a9bd8d9e68a66c2da05777bf01bbc20183b1cd8
shahirsizan <shahir.sizan18@gmail.com>
2025-08-21 22:39:40 +0600

    Steps component done

--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { assets } from "../assets/assets";
 
 const Steps = () => {
-	return <div>Steps</div>;
+	return (
+		<div className="px-4 lg:px-44 py-20 xl:py-40">
+			<h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold text-center">
+				Steps to remove{" "}
+				<span className="text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500">
+					background
+				</span>
+			</h1>
+
+			{/* <div className="flex items-start flex-wrap gap-4 mt-16 xl:mt-24 justify-center"> */}
+			<div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-16 xl:mt-24 justify-center text-lg">
+				<div className="flex flex-col items-center justify-center gap-2 sm:gap-4 bg-white rounded-lg drop-shadow-lg p-4 sm:p-5">
+					<img src={assets.upload_icon} className="max-w-9" />
+					<div>
+						<p className="">Upload image</p>
+					</div>
+				</div>
+				<div className="flex flex-col items-center justify-center gap-2 sm:gap-4 bg-white rounded-lg drop-shadow-lg p-4 sm:p-7">
+					<img src={assets.remove_bg_icon} className="max-w-9" />
+					<div>
+						<p className="">Remove background</p>
+					</div>
+				</div>
+				<div className="flex flex-col items-center justify-center gap-2 sm:gap-4 bg-white rounded-lg drop-shadow-lg p-4 sm:p-7">
+					<img src={assets.download_icon} className="max-w-9" />
+					<div>
+						<p className="">Download image</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Steps;
