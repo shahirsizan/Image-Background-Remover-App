77843292b14648ed84b7ff9a7c2abdbaf9d969c3
shahirsizan <shahir.sizan18@gmail.com>
2025-08-21 19:53:03 +0600

    Initial project structure set up

---
+++ client/src/components/BgSlider.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BgSlider = () => {
+	return <div>BgSlider</div>;
+};
+
+export default BgSlider;
---
+++ client/src/pages/BuyCredits.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BuyCredits = () => {
+	return <div>BuyCredits</div>;
+};
+
+export default BuyCredits;
