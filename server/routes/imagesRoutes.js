c0b7eb558ff456db125349e064863188582a89ad
shahirsizan <shahir.sizan18@gmail.com>
2025-08-25 21:09:03 +0600

    Multer middleware added in backend

--- server/routes/imagesRoutes.js
+++ server/routes/imagesRoutes.js
@@ -3,6 +3,14 @@ import authUser from "../middlewares/auth.js";
 
 const imageRouter = express.Router();
 
-imageRouter.post(`/remove-bg`, authUser, removeBgImageControllerImplementLater);
+imageRouter.post(`/remove-bg`, upload.single("image"), authUser, removeBgImage);
+/**
+ * Above single("image") returns middleware that processes a single file associated with the
+ * given form field.
+ * The `request` object will be populated with a `file` object containing
+ * information about the processed file.
+ * @param fieldName Name of the multipart form field to process.
+ */
+// Signature >>> single(fieldName: string): RequestHandler;
 
 export default imageRouter;
