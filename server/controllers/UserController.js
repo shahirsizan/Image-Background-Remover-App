44f70ff1b134ab67b7ed8e7a5681eb3366e5f043
shahirsizan <shahir.sizan18@gmail.com>
2025-08-28 10:46:08 +0600

    slight modification

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -61,83 +61,15 @@ const clerkWebhooks = async (req, res) => {
 	}
 };
 
-// API to get user available credits
 const userCredits = async (req, res) => {
-	// console.log("ami UserController->userCredits e 1");
-
 	try {
-		const { clerkId } = req.headers;
-		const userData = await userModel.findOne({ clerkId: clerkId });
+		const { user } = req;
 		// console.log("userData :>> ", userData);
-		res.json({ success: true, userCredits: userData.creditBalance });
+		res.json({ success: true, userCredits: user.creditBalance });
 	} catch (error) {
 		console.log("error :>> ", error.message);
 		res.json({ success: false, message: error.message });
 	}
 };
-
-// API to make payment for credits (copied from video tutorial repo)
-// const paymentBkashpay = async (req, res) => {
-// 	try {
-// 		const { clerkId } = req.headers;
-// 		const { planId } = req.body;
-
-// 		const userData = await userModel.findOne({ clerkId: clerkId });
-
-// 		if (!userData || !planId) {
-// 			return res.json({ success: false, message: "Invalid Credentials" });
-// 		}
-
-// 		let credits, plan, amount, database;
-// 		switch (planId) {
-// 			case "Basic":
-// 				plan = "Basic";
-// 				credits = 100;
-// 				amount = 10;
-// 				break;
-// 			case "Advanced":
-// 				plan = "Basic";
-// 				credits = 500;
-// 				amount = 50;
-// 				break;
-// 			case "Business":
-// 				plan = "Basic";
-// 				credits = 5000;
-// 				amount = 250;
-// 				break;
-
-// 			default:
-// 				break;
-// 		}
-
-// 		date = Date.now();
-
-// 		const transactionData = {
-// 			clerkId,
-// 			plan,
-// 			amount,
-// 			credits,
-// 			date,
-// 		};
-
-// 		const newTransaction = await transactionModel.create(transactionData);
-
-// 		const options = {
-// 			amount: amount * 100,
-// 			currency: process.env.CURRENCY,
-// 			receipt: newTransaction._id,
-// 		};
-
-// 		await razorpayInstance.orders.create(options, (error, order) => {
-// 			if (error) {
-// 				res.json({ success: false, message: error });
-// 			}
-// 			res.json({ success: true, order });
-// 		});
-// 	} catch (error) {
-// 		console.log("error :>> ", error.message);
-// 		res.json({ success: false, message: error.message });
-// 	}
-// };
 
 export { clerkWebhooks, userCredits };
